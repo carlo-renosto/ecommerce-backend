0eea80701d12dbdec94063ddc66943be3fc43dae
carlo-renosto <carlorenosto13@gmail.com>
2025-10-22 19:54:29 -0300

    Corregir y añadir casos de testing

--- test/cartsDao.test.js
+++ test/cartsDao.test.js
@@ -1,36 +1,64 @@
 
-import { expect } from 'chai';
-import mongoose from "mongoose";
-import { cartManagerMongo } from "../src/dao/managers/mongo/cart.mongo.js" 
+// Correr con:
+// npx mocha test/cartsDao.test.js
 
-try {
-    await mongoose.connect("mongodb+srv://ecmrc:BL99hsn2ZtvV8I9b@ecommerce.rowzcke.mongodb.net/?retryWrites=true&w=majority");
-} 
-catch(error) {
-    console.log(error.message);
-}
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { expect } from 'chai';
+import { cartManagerMongo } from "../src/dao/managers/mongo/cart.mongo.js";
+
+let mongoServer;
+let cartManager;
+
+before(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+  cartManager = new cartManagerMongo();
+});
+
+after(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
 
 describe('Carts DAO - Pruebas', () => {
     const cartManager = new cartManagerMongo();
+    var cid = 0;
+    var cart = undefined;
 
-    it('El DAO de carritos permite crear un nuevo carrito', async () => {
-        const uid = "6584f6713252cb25a597267e"
+    it('Crear un nuevo carrito', async () => {
+        const uid = "65b0773bce14ee1278c2dc51"
 
         const createdCart = await cartManager.createCart(uid);
+        cid = createdCart.id;
+        cart = createdCart;
         expect(createdCart).to.have.property('_id');
     });
 
-    it('El DAO de carritos permite obtener un carrito existente', async () => {
-        const cid = '6567bfc1c5873616d95bd513';
-
+    it('Obtener un carrito existente', async () => {
         const cart = await cartManager.getCartById(cid, false);
         expect(cart).to.be.an('object');
     });
 
-    
-    it('El DAO de carritos permite vaciar un carrito', async () => {
-        const cid = '6567bfc1c5873616d95bd513';
+    it('Actualizar un carrito existente', async () => {
+        const newCart = {
+            _id: cart.id,
+            _uid: "65b0773bce14ee1278c2dc51",
+            products: [
+                {
+                    _id: "65b0773bce14ee1278c34d52",
+                    title: "Producto",
+                    description: "Descripción"
+                }
+            ]
+        }
 
+        const updatedCart = await cartManager.updateCart(newCart._id, newCart);
+        expect(updatedCart).to.be.an('object');
+    });
+    
+    it('Vaciar un carrito existente', async () => {
         var cart = await cartManager.getCartById(cid, false);
         cart.products = [];
 
--- test/productsDao.test.js
+++ test/productsDao.test.js
@@ -1,43 +1,65 @@
 
-import { expect } from 'chai';
-import mongoose from "mongoose";
-import { productManagerMongo } from "../src/dao/managers/mongo/product.mongo.js" 
+// Correr con:
+// npx mocha test/productsDao.test.js
 
-try {
-    await mongoose.connect("mongodb+srv://ecmrc:BL99hsn2ZtvV8I9b@ecommerce.rowzcke.mongodb.net/?retryWrites=true&w=majority");
-} 
-catch(error) {
-    console.log(error.message);
-}
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { expect } from 'chai';
+import { productManagerMongo } from "../src/dao/managers/mongo/product.mongo.js";
+
+let mongoServer;
+let productManager;
+
+before(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+  productManager = new productManagerMongo();
+});
+
+after(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
 
 describe('Products DAO - Pruebas', () => {
     const productManager = new productManagerMongo();
+    var pid = 0;
+    var product = undefined;
 
-    it('El DAO de productos permite crear un nuevo producto', async () => {
+    it('Crear un nuevo producto', async () => {
         const productInfo = {
             title: 'Test Product',
             description: 'This is a test product',
-            code: 'abc160',
-            price: 19.20,
+            code: 'ABC123',
+            price: 20,
             stock: 10,
-            category: 'Tecnologia',
+            category: 'Technology',
             owner: 'admin',
             thumbnail: 'test-thumbnail-url',
         };
 
         const createdProduct = await productManager.createProduct(productInfo);
+        pid = createdProduct._id;
+        product = createdProduct;
         expect(createdProduct).to.have.property('_id');
     });
 
-    it('El DAO de productos permite obtener un arreglo con todos los productos', async () => {
+    it('Obtener array de productos existentes', async () => {
         const products = await productManager.getProducts();
         expect(products).to.be.an('array');
     });
 
-    it('El DAO de productos permite obtener un producto existente', async () => {
-        const pid = '652743059d6678e8ba28ce0d';
-
+    it('Obtener un producto existente', async () => {
         const product = await productManager.getProductById(pid);
         expect(product).to.be.an('object');
+    });
+
+    it('Actualizar un producto existente', async () => {
+        product.price = 40;
+        product.stock = 100;
+
+        const updatedProduct = await productManager.updateProduct(pid, product)
+        expect(updatedProduct).to.be.an('object');
     });
 });
